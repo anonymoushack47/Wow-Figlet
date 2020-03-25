9227e43288f999dedc8352e18411e7e255e2e541
anonymoushack47 <59957630+anonymoushack47@users.noreply.github.com>
2020-03-25 15:03:31 +0545

    Update file.sh

--- file.sh
+++ file.sh
@@ -5,26 +5,19 @@ cd
 echo "\033[1;93m#welcome"
 figlet  "Hello"
 echo ""
-echo "$(tput setaf 5) Subscribe my Anonymous Hack47 YT channel For more Termux videos "
+echo "Subscribe my Anonymous Hack47 YT channel For more Termux videos "
 echo ""
-echo "$(tput setaf 10) Installing FIGLET......."
-apt install figlet -y
-echo "$(tput setaf 6) Installing TOILET......."
-apt install toilet -y
+echo "Installing FIGLET......."
+echo "Installing TOILET......."
 echo ""
-echo "$(tput setaf 10) Installing COWSAY......."
+echo "Installing COWSAY......."
 figlet -f Bubble "installing cowsay"
-apt install cowsay -y
 echo ""
-echo "$(tput setaf 14) Installing NODE-JS......."
+echo "Installing NODE-JS......."
 figlet -f Bubble "installing NodeJs"
-apt install nodejs -y
 echo ""
-echo "$(tput setaf 5) Installing FIGLET-cli......."
-npm install figlet 
-
+echo "Installing FIGLET-cli......."
 figlet -f Bubble "installing figlet CLI"
-npm install -g figlet-cli
 echo ""
 echo "\033[1;91m"
 
@@ -32,10 +25,10 @@ cowsay "Finished"
 
 figlet -f small "Done!"
 
-echo "$(tput setaf 10) installation Succesful [DONE!]"
-echo "|-----------------------------|"
-echo "$(tput setaf 3) Subscribe Anonymous Hack47.."
-echo "|-----------------------------|"
+echo "\e[1;92mInstallation Succesful [DONE!]"
+echo "\e[1;93m|-----------------------------|"
+echo "\e[1;92mSubscribe Anonymous Hack47.."
+echo "\e[1;93m|-----------------------------|"
 echo ""
-echo "Now Run This Tool."
+echo "\e[1;95mNow Run This Tool."
 cd Wow-Figlet
